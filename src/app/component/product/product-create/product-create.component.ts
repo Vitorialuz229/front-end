107a186b85270af83c08a04c1261aa92a2db584d
Vitorialuz229 <vitorialuz831@gmail.com>
2024-09-25 14:06:37 -0300

    :sparkles: feat:  Adicionado método createProduct e navegação ao criar produto

--- src/app/component/product/product-create/product-create.component.ts
+++ src/app/component/product/product-create/product-create.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+
+import { ProductService } from '../../service/product.service';
+import { Product } from '../../interface/product';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatCardModule, MatInputModule, MatButtonModule, FormsModule],
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css'
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    nome_do_sensor: "",
+    localizacao: "",
+    tipo_de_sensor: ""
+  }
+
+  constructor(private productService: ProductService, private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  createProduct(): void {
+    this.productService.create(this.product)
+      .subscribe(() => {
+        this.productService.showMessage("Operação realizada com sucesso")
+        this.router.navigate(['/sensor'])
+      })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/sensor'])
+  }
 }
