c481edd06372b4bf28e5e0996ae9d049c3b59311
Vitorialuz229 <vitorialuz831@gmail.com>
2024-09-25 18:11:54 -0300

    :sparkles: feat: Criar roteamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './component/view/home/home.component';
 import { CrudComponent } from './component/view/crud/crud.component';
 import { ContainerTitleComponent } from './component/template/container-title/container-title.component';
 import { ProductCreateComponent } from './component/product/product-create/product-create.component';
+import { ProductReadComponent } from './component/product/product-read/product-read.component';
 
 export const routes: Routes = [
   {
@@ -13,5 +14,8 @@ export const routes: Routes = [
   },
   {
     path: 'sensor/create', component: ProductCreateComponent
+  },
+  {
+    path: 'sensor/update:id', component: ProductReadComponent
   }
 ];
