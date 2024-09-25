35a5b3426022be99a96406e7f396001cc9cfbec0
Vitorialuz229 <vitorialuz831@gmail.com>
2024-09-25 13:36:47 -0300

    :sparkles: feat: Criando roteamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/view/home/home.component';
+import { CrudComponent } from './component/view/crud/crud.component';
+import { ContainerTitleComponent } from './component/template/container-title/container-title.component';
+import { ProductCreateComponent } from './component/product/product-create/product-create.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: ContainerTitleComponent
+  },
+  {
+    path: 'sensor', component: CrudComponent
+  },
+  {
+    path: 'sensor/create', component: ProductCreateComponent
+  }
+];
